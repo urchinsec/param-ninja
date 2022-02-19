1fbdb3dfa4273776c368210d6b40134dbc5fdf43
Tahaa Farooq <49201347+tahaafarooq@users.noreply.github.com>
2022-02-19 17:37:24 +0300

    updated flask web server

--- api_ninja.py
+++ api_ninja.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_bootstrap import Bootstrap
 from scanner import Scanner
+
+import json
 
 scanner = Scanner()
 
@@ -9,11 +11,20 @@ Bootstrap(app)
 
 @app.route("/")
 def index():
-  return render_template("index.html")
+    return render_template("index.html")
 
-@app.route("/check", methods = ["POST"])
+@app.route("/check", methods=["POST"])
 def check_url():
-  
-  check_result = scanner.full_scan(request.form["url"])
+    check_result = scanner.full_scan(request.form["url"])
+    return render_template("index.html", check_result=check_result)
 
-  return render_template("index.html", check_result=check_result)
+@app.route("/output", methods=["GET"])
+def output():
+    with open("output_exploit_search.json",'r') as exploitResult:
+        read = exploitResult.readlines()
+        read = jsonify(read)
+        
+        return read
+
+if __name__ == "__main__":
+    app.run()
