8f2b5f9cf2726886625be4f6de97659fa73d4b8c
Dmitry Mineev <dmitry.mineev@gmail.com>
2022-03-08 13:08:55 +0100

    Recover deleted endpoint and make them more secure

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, jsonify
 from flask_login import login_required, current_user
 from scanner import Scanner
 
@@ -11,8 +11,18 @@ def index():
     return render_template("index.html", current_user = current_user)
 
 @main.route("/check", methods = ["POST"])
+@login_required
 def check_url():
   
   check_result = scanner.full_scan(request.form["url"])
 
   return render_template("index.html", check_result=check_result)
+
+@main.route("/output", methods=["GET"])
+@login_required
+def output():
+    with open("output_exploit_search.json",'r') as exploitResult:
+        read = exploitResult.readlines()
+        read = jsonify(read)
+        
+        return read
