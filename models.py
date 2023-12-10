47a1eda5bddfd8f092b54b4aed9d7b78db973788
tahaafarooq <tahacodez@gmail.com>
2023-12-10 23:50:37 +0300

    Updated the Database Model and removed the original database

--- models.py
+++ models.py
@@ -1,8 +1,45 @@
 from flask_login import UserMixin
 from starter import db
 
+
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    __tablename__ = "param_ninja_users"
+    id = db.Column(db.Integer, primary_key=True)  # primary keys are required by SQLAlchemy
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
-    name = db.Column(db.String(1000))
+    username = db.Column(db.String(100))
+
+    def __init__(self, email, password, username):
+        self.email = email
+        self.password = password
+        self.username = username
+
+
+class ScanLog(db.Model):
+    __tablename__ = "param_ninja_scans"
+    id = db.Column(db.Integer, primary_key=True)
+    domain = db.Column(db.String(80))
+    username = db.Column(db.String())
+    scan_output = db.Column(db.String())
+    output_file = db.Column(db.String())
+    time_scan = db.Column(db.String())
+
+    def __init__(self, domain, username, scan_output, output_file, time_scan):
+        self.domain = domain
+        self.username = username
+        self.scan_output = scan_output
+        self.output_file = output_file
+        self.time_scan = time_scan
+
+
+class ReportLog(db.Model):
+    __tablename__ = "param_ninja_reports"
+    id = db.Column(db.Integer, primary_key=True)
+    domain = db.Column(db.String())
+    username = db.Column(db.String())
+    report_path = db.Column(db.String())
+
+    def __init__(self, domain, username, report_path):
+        self.domain = domain
+        self.username = username
+        self.report_path = report_path
